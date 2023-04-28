02d3780a9d6485647de768d50a91e6cf235c60a4
Souvik Guha <svkguha123@gmail.com>
2023-04-28 13:46:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function createTable() {
     //Write your code here
+    rows = window.prompt("Input number of rows", 1);
+    columns = window.prompt("Input number of columns",1);
   
+    for(var r=0;r<parseInt(rows, 10);r++) {
+		
+        var x=document.getElementById('myTable').insertRow(r);
+		
+        for(var c=0;c<parseInt(columns, 10);c++) {
+			
+            var y=  x.insertCell(c);
+			
+            y.innerHTML="Row-"+r+" Column-"+c; 
+        }
+    }
 }
